ae8e56d0370cad877ebd84f163a44f26b3a1e88d
Rodrigo Ferrari <ferrari_rod@hotmail.com>
2022-09-05 22:59:23 -0300

    documenting saveLog function

--- src/js/packages/logSaver.js
+++ src/js/packages/logSaver.js
@@ -2,9 +2,11 @@ import { Dropbox } from "dropbox"
 const ACCESS_TOKEN = ""
 
 /**
- * 
- * @param {string} data 
- * @param {string} path 
+ * A function that saves the log of the levels on a Dropbox server (a file called log.txt),generates one file per play, needs to put the access token on the file to work. Returns a boolean to indicate if the upload was a success.
+ * @async
+ * @param {string} data - The data that will be writen in the log.txt
+ * @param {string} path - The path that the file will be saved in the app folder, it's recommended to call the path by the level name, such as '/level1/'
+ * @returns {Promise<boolean>}
  */
 export async function saveLog(data,path)
 {
