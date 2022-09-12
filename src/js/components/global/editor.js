906ca491d805e3945f83b8177c7be902997bf5b1
Rodrigo Ferrari <ferrari_rod@hotmail.com>
2022-09-12 12:27:35 -0300

    adding a minheight in the editor

--- src/js/components/global/editor.js
+++ src/js/components/global/editor.js
@@ -5,15 +5,18 @@ import {indentWithTab} from "@codemirror/commands"
 
 var codeArea = document.getElementById("codeToExecute")
 
+const minHeightEditor = EditorView.theme({
+    ".cm-content, .cm-gutter": {minHeight: "200px"}
+})
+
 /**
  * The object of the CodeMirror's code editor in the phase. 
  * @constant {EditorView}
  */
 export const editor = new EditorView({
-    extensions: [basicSetup,keymap.of([indentWithTab])],
+    extensions: [basicSetup, minHeightEditor,keymap.of([indentWithTab])],
     parent: codeArea,
 })
-
 /**
  * A Codemirror's editor state configured to read only.
  * @constant {EditorState}
