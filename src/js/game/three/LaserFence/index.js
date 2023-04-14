0c579074061e851dd19f9166df2b53defc423b4d
andrelbdutra <bazulylbd@gmail.com>
2023-04-14 02:23:56 -0300

    colocando o laser atualizado

--- src/js/game/three/LaserFence/index.js
+++ src/js/game/three/LaserFence/index.js
@@ -4,13 +4,25 @@ import { CSG } from '../FireBase/CSGMesh';
 
 class Fence extends THREE.Mesh {
     constructor() { 
-        super(new THREE.BoxGeometry(0.5, 2, 0.3), new THREE.MeshLambertMaterial({color:"gray"}));
+        super(new THREE.BoxGeometry(0.5, 2, 0.15), new THREE.MeshLambertMaterial({color:"white"}));
+    }
+};
+
+class FenceTorus extends THREE.Mesh {
+    constructor() { 
+        super(new THREE.TorusGeometry(0.15, 0.05 , 10, 20), new THREE.MeshLambertMaterial({color:"black"})); 
+    }
+};
+
+class FenceBase extends THREE.Mesh {
+    constructor() { 
+        super(new THREE.BoxGeometry(0.5, 2, 0.1), new THREE.MeshLambertMaterial({color:"white"}));
     }
 };
 
 class Laser extends THREE.Mesh {
     constructor(color) { 
-        super(new THREE.CylinderGeometry(0.1, 0.1, 2, 64, 64), new THREE.MeshLambertMaterial({color:color, transparent:true, opacity: 0.5}));
+        super(new THREE.CylinderGeometry(0.1, 0.1, 2, 64, 64), new THREE.MeshLambertMaterial({emissive:color, color:color, emissiveIntensity:1, transparent:true, opacity: 0.7}));
     }
 };
 
@@ -24,11 +36,30 @@ class LaserFence extends THREE.Object3D {
         this.active = true
         this.type = type;
 
+        // fence base
+        let fenceBase = new FenceBase;
+        fenceBase.rotateX(-Math.PI / 2)
+        fenceBase.position.set(0, -0.95, 0)
+
         // fences
         let laserFence1 = new Fence;
-        laserFence1.position.set(0, 0, -1)
+        laserFence1.position.set(0, 0, -0.93)
         let laserFence2 = new Fence;
-        laserFence2.position.set(0, 0, 1)
+        laserFence2.position.set(0, 0, 0.93)
+
+        // fence torus
+        let fenceTorus1A = new FenceTorus;
+        fenceTorus1A.position.set(0, 0.6, 0.85);
+        let fenceTorus1B = new FenceTorus;
+        fenceTorus1B.position.set(0, 0.6, -0.85);
+        let fenceTorus2A = new FenceTorus;
+        fenceTorus2A.position.set(0, 0, 0.85);
+        let fenceTorus2B = new FenceTorus;
+        fenceTorus2B.position.set(0, 0, -0.85);
+        let fenceTorus3A = new FenceTorus;
+        fenceTorus3A.position.set(0, -0.6, 0.85);
+        let fenceTorus3B = new FenceTorus;
+        fenceTorus3B.position.set(0, -0.6, -0.85);
 
         // blue lasers
         let laserBlue1 = new Laser("blue")
@@ -62,8 +93,15 @@ class LaserFence extends THREE.Object3D {
             this.blueLasers.forEach(laser => laser.visible = false)
         }
 
+        this.add(fenceBase);
         this.add(laserFence1);
         this.add(laserFence2);
+        this.add(fenceTorus1A);
+        this.add(fenceTorus1B);
+        this.add(fenceTorus2A);
+        this.add(fenceTorus2B);
+        this.add(fenceTorus3A);
+        this.add(fenceTorus3B);
         this.add(laserBlue1);
         this.add(laserBlue2);
         this.add(laserBlue3);      
